2a8e8829fd2290ddaba8bd13223b0b582a9bd29c
Alexander Dittrich <alexander.dittrich@me.com>
2022-12-04 16:41:09 +0100

    Day 4

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,10 +1,23 @@
+typealias Elf = IntRange
+
 object Day4 : PuzzleSolver(4) {
 
-    override fun solve1(input: String): Number {
-        return 0
-    }
+    override fun solve1(input: String) = input.lines()
+        .map { it.toElvesPair() }
+        .count { (first, second) -> first.contains(second) || second.contains(first) }
 
-    override fun solve2(input: String): Number {
-        return 0
-    }
+    override fun solve2(input: String) = input.lines()
+        .map { it.toElvesPair() }
+        .count { (first, second) -> first.tailOverlapsWith(second) || second.tailOverlapsWith(first) }
+
+    private fun String.toElvesPair(): Pair<Elf, Elf> = split(",")
+        .map { elf -> elf.toElf() }
+        .let { (first, second) -> first to second }
+
+    private fun String.toElf() = split("-")
+        .map { it.toInt() }
+        .let { (first, second) -> Elf(first, second) }
+
+    private fun Elf.contains(other: Elf) = this.first <= other.first && this.last >= other.last
+    private fun Elf.tailOverlapsWith(other: Elf) = this.first >= other.first && this.first <= other.last
 }
