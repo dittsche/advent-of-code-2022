08fbc9685ed82dee42b2483c62544692574c452c
Alexander Dittrich <alexander.dittrich@me.com>
2022-12-06 18:47:01 +0100

    Day 6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,10 +1,10 @@
 object Day6 : PuzzleSolver(6) {
 
-    override fun solve1(input: String): Number {
-        return 0
-    }
+    override fun solve1(input: String) = input.findStartOfWithLength(4)
 
-    override fun solve2(input: String): Number {
-        return 0
-    }
+    override fun solve2(input: String) = input.findStartOfWithLength(14)
+
+    private fun String.findStartOfWithLength(length: Int) = windowed(length)
+        .first { it.toList().distinct().size == length }
+        .let { indexOf(it) + length }
 }
