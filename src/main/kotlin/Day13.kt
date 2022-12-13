df3c889bbd568ce05b7b07a7d014f95d13f712d4
Alexander Dittrich <alexander.dittrich@me.com>
2022-12-13 22:37:13 +0100

    Day 13

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,10 +1,80 @@
+import java.lang.System.lineSeparator
+import kotlin.math.min
+
 object Day13 : PuzzleSolver(13) {
 
-    override fun solve1(input: String): Number {
-        return 0
-    }
+    override fun solve1(input: String) = input.split(lineSeparator().repeat(2))
+        .mapIndexed { index, pair -> index to pair.toPacketPair() }
+        .filter { (_, pair) -> pair.first < pair.second }
+        .sumOf { (index, _) -> index + 1 }
 
     override fun solve2(input: String): Number {
-        return 0
+        val wrapped2 = 2.toPacket().wrapPacket()
+        val wrapped6 = 6.toPacket().wrapPacket()
+        return input.lines()
+            .filter { it.isNotBlank() }
+            .map { it.toPacket() }
+            .toMutableList()
+            .apply {
+                add(wrapped2)
+                add(wrapped6)
+            }
+            .sorted()
+            .let { (it.indexOf(wrapped2) + 1) * (it.indexOf(wrapped6) + 1) }
+    }
+
+    private fun String.toPacketPair() = lines()[0].toPacket() to lines()[1].toPacket()
+
+    private fun String.toPacket() = PacketParser.parsePacket(this)
+
+    private data class Packet(val int: Int?, val list: MutableList<Packet>?) : Comparable<Packet> {
+        override fun compareTo(other: Packet): Int = when {
+            int != null && other.int != null -> int - other.int
+            list != null && other.list != null -> (0 until min(list.size, other.list.size))
+                .asSequence()
+                .map { list[it].compareTo(other.list[it]) }
+                .find { it != 0 }
+                ?: (list.size - other.list.size)
+
+            list == null && other.int == null -> Packet(null, mutableListOf(this)).compareTo(other)
+            list != null && other.int != null -> this.compareTo(Packet(null, mutableListOf(other)))
+            else -> 0
+        }
+    }
+
+    private fun Int.toPacket() = Packet(this, null)
+    private fun Packet.wrapPacket() = Packet(null, mutableListOf(this))
+
+    private object PacketParser {
+        fun parsePacket(input: String): Packet {
+            val packets = mutableListOf<Packet>()
+            var lastPacket: Packet? = null
+            var numberCharBuffer = ""
+
+            val addNumberFromBuffer = {
+                if (numberCharBuffer.isNotEmpty()) {
+                    packets.last().list?.add(numberCharBuffer.toInt().toPacket())
+                    numberCharBuffer = ""
+                }
+            }
+
+            input.forEach {
+                when {
+                    it == '[' -> with(Packet(null, mutableListOf())) {
+                        packets.lastOrNull()?.list?.add(this)
+                        packets.add(this)
+                    }
+
+                    it == ']' -> {
+                        addNumberFromBuffer()
+                        lastPacket = packets.removeLast()
+                    }
+
+                    it == ',' -> addNumberFromBuffer()
+                    it.isDigit() -> numberCharBuffer += it
+                }
+            }
+            return lastPacket!!
+        }
     }
 }
--- src/test/kotlin/PuzzleSolverTest.kt
+++ src/test/kotlin/PuzzleSolverTest.kt
@@ -19,7 +19,7 @@ class Day10Test : AbstractPuzzleSolverTest(Day10, 13140, "\n##  ##  ##  ##  ##  
         "#######       #######       #######     ")
 class Day11Test : AbstractPuzzleSolverTest(Day11, 10605, 2713310158)
 class Day12Test : AbstractPuzzleSolverTest(Day12, 31, 29)
-class Day13Test : AbstractPuzzleSolverTest(Day13, -1, -1)
+class Day13Test : AbstractPuzzleSolverTest(Day13, 13, 140)
 class Day14Test : AbstractPuzzleSolverTest(Day14, -1, -1)
 class Day15Test : AbstractPuzzleSolverTest(Day15, -1, -1)
 class Day16Test : AbstractPuzzleSolverTest(Day16, -1, -1)
