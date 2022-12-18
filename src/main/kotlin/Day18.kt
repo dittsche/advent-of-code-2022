6b8d588da001dd24ad818ea66be1a4b0a0a24511
Alexander Dittrich <alexander.dittrich@me.com>
2022-12-18 17:44:29 +0100

    Day 18.1

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,10 +1,27 @@
 object Day18 : PuzzleSolver(18) {
 
-    override fun solve1(input: String): Number {
-        return 0
-    }
+    override fun solve1(input: String) = input.lines()
+        .map { it.toCube() }
+        .exposedSides()
+        .count()
 
     override fun solve2(input: String): Number {
         return 0
+    }
+
+    private fun List<Cube>.exposedSides() = this.flatMap { it.adjacentCubes() } - this.toSet()
+
+    private fun String.toCube() = splitToInts(",")
+        .let { (x, y, z) -> Cube(x, y, z) }
+
+    private data class Cube(val x: Int, val y: Int, val z: Int) {
+        fun adjacentCubes() = listOf(
+            Cube(x - 1, y, z),
+            Cube(x + 1, y, z),
+            Cube(x, y - 1, z),
+            Cube(x, y + 1, z),
+            Cube(x, y, z - 1),
+            Cube(x, y, z + 1),
+        )
     }
 }
